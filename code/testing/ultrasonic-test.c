24814d1488a52407c21cb50aa47ed95df6e5243a
leo <leoveenstra@gmail.com>
2013-04-11 16:02:06 +0200

    added ultrasonic test program

--- code/testing/ultrasonic-test.c
+++ code/testing/ultrasonic-test.c
@@ -1,13 +1,10 @@
-// Testing program to get values from the ultrasonic sensor
+#pragma config(Sensor, S4,sonarSensor,sensorSONAR) // configurating the ultrasonic sensor
 
-// Config the type of sensor on the specific port
-#pragma config(Sensor, S4,sonarSensor,sensorSONAR)
-
-task main(){
+task main()
+{
 	eraseDisplay();
-	while(1){
-		// Display the values from the ultrasonic sensor on the display
-		nxtDisplayString(1, "%d", SensorValue[sonarSensor]);
+  while(1){
+  	nxtDisplayString(1, "%d", SensorValue[sonarSensor]);
   	wait1Msec(50);
   	eraseDisplay();
 	}
